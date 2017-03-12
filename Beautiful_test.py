99f48ba7c16e09b24490dd0611387f55a8722924
kotarokawamura <kotaro.kwmr@gmail.com>
2017-03-12 17:16:42 +0900

    Name Change

--- Beautiful_test.py
+++ Beautiful_test.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-from func.read_html import read_html
+from func.get_bs4 import read_soup
+import sys
 
-test = read_html('http://himado.in/')
-a = test.obj_read()
+#path = sys.args[1]
+
+test = read_soup('https://en.wikipedia.org/')
+
+bs4_ins = test.bs4obj_ins()
 print(test.read_title())
--- func/get_bs4.py
+++ func/get_bs4.py
@@ -6,7 +6,7 @@ from urllib.error import HTTPError
 from urllib.error import URLError
 from bs4 import BeautifulSoup
 
-class read_html():
+class read_soup():
     def __init__(self, path):
         self.path = path
         self.html = self.open_url()
@@ -28,17 +28,22 @@ class read_html():
 
         return None
     
-    def soup_read(self):
-        ''' Create Ins Beautifulsoup4
+    def bs4obj_ins(self, path=None):
+        ''' Create Ins (Beautifulsoup4
             return ins / None
         '''
+
+        if path is not None:
+            self.path = path
+            self.open_url()
+            
 
         self.bsObj = BeautifulSoup(self.html.read(), "html5lib")
         return self.bsObj
 
     def read_title(self):
-        if soup_read is None:
-            self.bsObj = self.soup_read()
+        '''testets
+        '''
         
         try:
             title = self.bsObj.head.title
@@ -50,7 +55,7 @@ class read_html():
 
 
 if __name__ == '__main__':
-    test = read_html('http://himado.in/')
-    a = test.soup_read()
+    test = read_soup('http://himado.in/')
+    a = test.bs4obj_read()
     print(test.read_title())
         
